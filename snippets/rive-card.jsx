9dc34973ba191249dc919f9f2573cb17cc25a781
Lance <lance@lancesnider.com>
2026-05-18 15:38:55 -0700

    add graphics

--- snippets/rive-card.jsx
+++ snippets/rive-card.jsx
@@ -1,4 +1,4 @@
-export const RiveCard = ({ title, description, links, children, source }) => {
+export const RiveCard = ({ title, description, links, children, source, aspect3by2}) => {
 
   const runtimesInOrder = ['web', 'react', 'reactNative', 'flutter', 'apple', 'android', 'unity', 'unreal']
   const runtimeTitles = {
@@ -31,11 +31,21 @@ export const RiveCard = ({ title, description, links, children, source }) => {
       className="flex flex-col card block font-normal group relative my-2 ring-2 ring-transparent rounded-2xl bg-white dark:bg-background-dark border border-gray-950/10 dark:border-white/10 overflow-hidden w-full"
     >
 
-      <div className="w-full h-0 relative pb-[75%]">
-        <div className="absolute inset-0">
-          { children }
-        </div>
-      </div>
+      {
+        aspect3by2 ? (
+          <div className={`w-full h-0 relative pb-[66%]`}>
+            <div className="absolute inset-0">
+              { children }
+            </div>
+          </div>
+        ): (
+          <div className={`w-full h-0 relative pb-[75%]`}>
+            <div className="absolute inset-0">
+              { children }
+            </div>
+          </div>
+        )
+      }
 
       <div className="flex flex-grow flex-col px-6 py-5 relative" data-component-part="card-content-container">
       <div className="flex flex-col grow">
@@ -65,7 +75,7 @@ export const RiveCard = ({ title, description, links, children, source }) => {
           </div>
             <div className="mt-6 flex flex-wrap">
               {
-                runtimesInOrder.map((currentRuntime) => {
+                links && runtimesInOrder.map((currentRuntime) => {
                   return (
                     <RuntimeLink key={currentRuntime} runtime={currentRuntime} link={links[currentRuntime]} />
                   )
