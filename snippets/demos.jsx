0e3fe0e4f1350214232c83455391e5278cdfe149
Lance <lance@lancesnider.com>
2026-06-30 15:29:28 -0700

    conflict 1

--- snippets/demos.jsx
+++ snippets/demos.jsx
@@ -20,6 +20,15 @@ export const Demos = ({
         editor: "https://rive.app/community/files/28080-53039-ik-constraint/",
       }
     },
+    accessibilityReducedMotion: {
+      title: 'Reduced Motion Example',
+      description: 'Strategies for respecting a user\'s reduced motion preferences.',
+      riv: 'https://static.rive.app/rivs/accessibility_reduced_motion.riv',
+      stateMachines: "State Machine 1",
+      links: {
+        editor: "https://rive.app/community/files/28077-53035-accessibility-reduced-momtion/"
+      },
+    },
     cachingARiveFile: {
       title: 'Caching a Rive File',
       description: 'Load the .riv into memory once, use it multiple times.',
