57c76b92411aae22c3d3b94f5573f690d56b4d2b
Lance <lance@lancesnider.com>
2026-08-06 10:29:31 -0700

    undo hover styling

--- snippets/rive-card.jsx
+++ snippets/rive-card.jsx
@@ -30,7 +30,7 @@ export const RiveCard = ({ title, description, links, children, source, aspect3b
 
   return (
     <div
-      className="card block font-normal group relative my-2 ring-2 ring-transparent rounded-2xl bg-white dark:mint-bg-background-dark border border-gray-950/10 dark:border-white/10 overflow-hidden w-full cursor-pointer hover:border-primary! dark:hover:border-primary-light!"
+      className="flex flex-col card block font-normal group relative my-2 ring-2 ring-transparent rounded-2xl bg-white dark:bg-background-dark border border-gray-950/10 dark:border-white/10 overflow-hidden w-full"
     >
 
       {
